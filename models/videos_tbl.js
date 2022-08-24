0bf1454542ee8b9d72a00e79abba8fe1e6075a5c
dikamjit_borah <dikamjit.borah@superprocure.com>
2022-08-25 01:05:13 +0530

    modify videos_tbl & generate migration

--- models/videos_tbl.js
+++ models/videos_tbl.js
@@ -8,14 +8,27 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   videos_tbl.init({
-    entry_id: DataTypes.INTEGER,
+    entry_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     video_id: DataTypes.STRING,
     title: DataTypes.STRING,
     description: DataTypes.STRING,
-    publish_time: DataTypes.DATE
+    publish_time: DataTypes.DATE,
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    }
   }, {
     sequelize,
     modelName: 'videos_tbl',
+    tableName: 'videos_tbl'
   });
   return videos_tbl;
 };
