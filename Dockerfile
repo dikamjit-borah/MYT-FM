1863b8c058e698ac29e512bf98729798162320a2
dikamjit_borah <dikamjit.borah@superprocure.com>
2022-08-26 18:43:33 +0530

    modify Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:16
-WORKDIR /usr/src/app
-COPY package*.json ./
+WORKDIR /app
+COPY package.json . 
 RUN npm install
-COPY . .
-EXPOSE 8082
-CMD [ "node", "server.js" ]
+COPY . ./
+EXPOSE 8081
+CMD [ "node", "server.js"]
 LABEL tag="MYT-FM"
--- config/_config.js
+++ config/_config.js
@@ -2,5 +2,5 @@ module.exports = {
     apiKeys: {
         googleCloud: [process.env.GC_API_KEY_3, process.env.GC_API_KEY_1, process.env.GC_API_KEY_2]
     },
-    apiCallInterval: 1800000 //30minutes
+    apiCallInterval: 300000 //1800000 - 30minutes
 }
